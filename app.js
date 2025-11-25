d13599740ba1aba02acfa01dbbc98e6830722529
Yonas Melkie <yonasmelkie@gmail.com>
2025-11-25 11:14:04 -0800

    define an ap route to get a single post using its id

--- app.js
+++ app.js
@@ -174,7 +174,26 @@ app.get ('/api/posts', async (req, res) => {
         console.error(err);
         res.status(500).json({error: 'database error'});
     }
-})
+});
+
+app.get ('/api/posts/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await db.query(
+            'SELECT * FROM posts WHERE post_id = $1', [id]
+        );
+
+        if (result.rows.length === 0){
+            return res.status(404).json({error: 'Post id not found'});
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch(err){
+        console.error(err);
+        res.status(500).json({error: 'database error'});
+    }
+});
 
 
 
