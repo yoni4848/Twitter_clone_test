88f8638b408c1213bd8ff932f28a009fe23ef811
Yonas Melkie <yonasmelkie@gmail.com>
2025-12-08 10:44:39 -0800

    add the endpoint for viewing posts form the account following

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 
-const PORT = 3000;
+const PORT = 3001;
 
 // Import routes
 const authRoutes = require('./routes/auth');
---
+++ routes/timeline.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const router = express.Router();
+const db = require('../database/db');
+const { authenticateToken } = require('../middlewares/auth');
+
+router.get('/', authenticateToken, async (req, res) => {
+    try{
+        const result = await db.query(
+            'SELECT posts.*, users.username, users.profile_picture FROM posts JOIN users ON posts.user_id = users.user_id WHERE posts.user_id IN (SELECT following_id FROM follows WHERE follower_id = $1) ORDER BY created_at DESC', [req.user_id]
+        );
+
+        res.status(200).json(result.rows);
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error: 'database error'});
+    }
+
+});
