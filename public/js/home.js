8a2ff523668fffcf87793e6972b11ee2333242ac
Yonas Melkie <yonasmelkie@gmail.com>
2025-12-17 11:20:25 -0800

    add the main structure for the look of the home page

--- public/js/home.js
+++ public/js/home.js
@@ -1,19 +1,80 @@
 const token = localStorage.getItem('token');
+const createPost = document.getElementById("create-post");
+const feed = document.getElementById("feed");
+const exploreButton = document.getElementById("explore-button");
+const feedButton = document.getElementById("feed-button");
 
-
-fetch('/api/timeline', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const feed = document.getElementById("feed");
-        data.forEach(element => {
-            feed.innerHTML += `<div class="post">
+//display posts inside post div
+function displayPosts(posts){
+    feed.innerHTML = "";
+    posts.forEach(element => {
+        feed.innerHTML += `<div class="post">
                             <p class="username">@${element.username}</p>
                             <p class="content">${element.content}</p>
                             </div>`
-        });
+    });
+}
+
+//fetch explore posts
+fetch('/api/explore', {
+        method: 'GET'
     })
+    .then(response => response.json())
+    .then(data => {
+        displayPosts(data);
+    })
+
+//create a new post
+createPost.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const postContent = document.getElementById("post-text-box");
+    fetch('/api/posts', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        },
+        body: JSON.stringify({content: postContent.value})
+    })
+    .then(response => response.json())
+  .then(data => {
+      postContent.value = "";
+      return fetch('/api/explore', {
+        method: "GET"
+      });
+  })
+  .then(response => response.json())
+  .then(data => {
+      displayPosts(data);
+  })
+
+});
+
+
+//show popular post when explore is clicked
+exploreButton.addEventListener("click", () => {
+    fetch("/api/explore", {
+        method: "GET"
+    })
+    .then (response => response.json())
+    .then(data => {
+        displayPosts(data);
+    })
+
+});
+
+//show posts from accounts followed when feed is clicked
+
+feedButton.addEventListener("click", () => {
+    fetch("/api/timeline", {
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer " + token
+        }
+    })
+    .then (response => response.json())
+    .then(data => {
+        displayPosts(data);
+    })
+
+});
