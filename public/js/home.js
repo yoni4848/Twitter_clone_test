0a973f8aa70344d2136f444888f62a2afd0a8c1a
Yonas Melkie <yonasmelkie@gmail.com>
2025-12-19 19:03:49 -0800

    style the homepage a bit better and add the functionality to log out

--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,16 @@ const createPost = document.getElementById("create-post");
 const feed = document.getElementById("feed");
 const exploreButton = document.getElementById("explore-button");
 const feedButton = document.getElementById("feed-button");
+const user = document.getElementById('user');
+const logoutButton = document.getElementById('log-out');
 
+//check if the user is logged in before letting access to the home page
+if (!token){
+    window.location.href = "../index.html";
+}
+    
+//display the username at the top right corner
+user.textContent += localStorage.getItem('username');
 
 //like a post
 function likePost(post_id){
@@ -85,6 +94,8 @@ createPost.addEventListener("submit", (event) => {
 
 //show popular post when explore is clicked
 exploreButton.addEventListener("click", () => {
+    feedButton.classList.remove('active');
+    exploreButton.classList.add('active');
     fetch("/api/explore", {
         method: "GET"
     })
@@ -98,6 +109,8 @@ exploreButton.addEventListener("click", () => {
 //show posts from accounts followed when feed is clicked
 
 feedButton.addEventListener("click", () => {
+    exploreButton.classList.remove('active');
+    feedButton.classList.add('active');
     fetch("/api/timeline", {
         method: "GET",
         headers: {
@@ -110,3 +123,9 @@ feedButton.addEventListener("click", () => {
     })
 
 });
+
+logoutButton.addEventListener("click", ()=>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    window.location.href = "../index.html"
+})
--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,16 @@
 const loginForm = document.getElementById("login-form");
 const userNameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
+const token = localStorage.getItem('token');
 
+
+//redirect to the home page if they are already logged in
+if (token){
+    window.location.href = "../home.html";
+}
+   
+
+//send the users username and password to backend
 loginForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const username = userNameInput.value;
@@ -17,10 +26,12 @@ loginForm.addEventListener("submit", (event) => {
     })
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         const errorMeassage = document.getElementById('error-message');
         if (data.error){
             errorMeassage.textContent = data.error;
         } else{
+            localStorage.setItem('username', data.user.username);
             localStorage.setItem('token', data.token);
             window.location.href = '../home.html';
         }
